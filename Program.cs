a4b13a4e373db14f4f661e41f773bac07ed87471
Diana Brik <di.brik@yandex.kz>
2023-01-17 22:34:52 +0600

    Seminar 14.01.23 Task 29

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(-10, 100);
+    }
+
+    return result;
+}
+
+const int sizeArray = 8;
+int[] array = GetArray(sizeArray);
+Console.WriteLine($"[{String.Join(", ", array)}]");
